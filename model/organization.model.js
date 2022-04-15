11f49ea7fc6ee902ffd5622b39a48b4b424af24f
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-04-15 22:51:37 +0530

    commited

---
+++ model/organization.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+
+
+const organizationSchema = new mongoose.Schema({
+    uuid:{type: String, required: false},
+    organizationName: {type: String, required: true, trim: true},
+    organizationDesc: {type: String, required: false, trim: true},
+    userUuid: {type: String, required: true}
+    },
+    {
+        timestamps: true
+    
+});
+
+// UUID generation
+organizationSchema.pre('save', function(next){
+    this.uuid = 'ORG-'+crypto.pseudoRandomBytes(6).toString('hex').toUpperCase()
+    console.log(this.uuid);
+    next();
+});
+
+module.exports=mongoose.model('organization',organizationSchema, 'organization');

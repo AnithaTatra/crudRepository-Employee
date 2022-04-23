c3100e827e3ca66ba4a1e3520d8492191462f149
AnithaTatra <102857170+AnithaTatra@users.noreply.github.com>
2022-04-23 22:11:16 +0530

    commited

--- middleware/email.js
+++ middleware/email.js
@@ -1,33 +1,44 @@
 const nodemailer = require('nodemailer')
+const ejs = require('ejs')
+const{join}= require('path');
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-     port: 465,
-     host:"smtp.gmail.com",
-     secure:true,
+    service: "gmail",
+  // port: 465,
+   //host:"smtp.gmail.com",
     auth:{
-        Username: "pinkyAngelQueen@gmail.com",
-        Password: "pinky@123"
+        user: "pinkyangelqueen123@gmail.com",
+        pass: "pinky@123"
     },
-   
+    //secure:false,
    
 });
 
 async function mailSending(compose){
-    //console.log("ok")
+    console.log("ok")
     try{
-        transporter.sendMail(compose,function(err){
+        
+        const data= await ejs.renderFile(join(__dirname,'../templates/',compose.fileName))
+        console.log(__dirname)
+        const mailData= {
+            from:compose.from,
+            to:compose.to,
+            subject:compose.subject,
+            attachments: compose.attachments,
+            html:data
+        }
+        transporter.sendMail(mailData,(err,data)=>{
            
             if(err){ 
                 console.log("err",err.message)
              }else{
                 console.log("Mail sent successfully")
-                return true
+                return 1
              }
         })
     }catch(error){
         console.log(error.message)
-        process.exit[1];
+        process.exit(1);
     }
 }
 module.exports = {mailSending}
